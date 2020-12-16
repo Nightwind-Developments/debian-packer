e3c5bf08c71f8dad95b272c218ad491807d348ad
Rowan Rathod <rowanrathod@nightwind-developments.co.nz>
2020-12-17 00:12:20 +1300

    Added Help Documentation
    Added better 'help' entries when the user uses the "--help" argument.

--- debianpacker.py
+++ debianpacker.py
@@ -9,21 +9,48 @@ import click
 APP_NAME = "Debian Packer"
 APP_AUTHOR = "Nightwind Developments"
 
+# Generic Package Name
+GENERIC_PKG_NAME = "package"
+
 # Default Location of File Map file
 DEFAULT_FILE_MAP = "example-map.json"
+HELP_FILE_MAP = "Path to the compatible JSON file with all the file mappings."
+
+# Default Location of Sources to Include in Package
+DEFAULT_INPUT_PATH = "./input"
+HELP_IP = "Path to directory with files to include in generated package."
+
+# Default Output Location for Package
+DEFAULT_OUTPUT_PATH = "./output"
+HELP_OP = "Path to directory where the package will be written to."
 
 # JSON Map File Keys
 KEY_NAME = "name"
 KEY_PATH = "path"
 
+# Debian Package File Extension
+FILE_EXT = ".deb"
+
+# File Name Separator
+FN_SEP = "_"
+
 # List of Mapped File Records
 mapped_files = list()
 
-# Target Architecture
-arch = str()
+# Package Application Name
+basic_name = str()
+HELP_PKG_NAME = "Base File Name for Package."
 
 # Version
 version = str()
+HELP_PKG_VER = "Package version number/code."
+
+# Target Architecture
+arch = str()
+HELP_PKG_ARCH = "Label of target architecture."
+
+# Full File Name of the Generated Package
+pkg_full_name = str()
 
 
 # Mapped File Record class
@@ -50,11 +77,26 @@ def print_all_details():
         mapped_files[i].print_details(str(i) + ".\t", "\t")
 
 
+# Generates File Name of the Package
+def get_package_name():
+    return basic_name + FN_SEP + version + FN_SEP + arch + FILE_EXT
+
+
 # Main Function to run on Start
 @click.command()
-@click.option('-m', '--file_map', type=click.Path('r'), default=DEFAULT_FILE_MAP)
-def main(file_map):
+@click.option('-n', '--pkg_name', type=click.STRING, default=GENERIC_PKG_NAME, help=HELP_PKG_NAME)
+@click.option('-v', '--pkg_version', type=click.STRING, help=HELP_PKG_VER)
+@click.option('-a', '--pkg_arch', type=click.STRING, help=HELP_PKG_ARCH)
+@click.option('-m', '--pkg_file_map', type=click.Path('r'), default=DEFAULT_FILE_MAP, help=HELP_FILE_MAP)
+@click.option('-i', '--input', type=click.Path('r'), default=DEFAULT_INPUT_PATH, help=HELP_IP)
+@click.option('-o', '--output', type=click.Path('w'), default=DEFAULT_OUTPUT_PATH, help=HELP_OP)
+def main(name, pkg_version, pkg_arch, file_map):
     click.echo("Welcome to " + APP_NAME + " by " + APP_AUTHOR + "\n")
+
+    # Saves the Package Variables
+    basic_name = name
+    version = pkg_version
+    arch = pkg_arch
 
     # Loads JSON File as a local variable
     with open(file_map) as f:
@@ -67,6 +109,8 @@ def main(file_map):
         mapped_files.append(Mapped(i))
 
     print_all_details()
+
+    print("Output File Name: " + get_package_name())
 
 
 if __name__ == "__main__":
